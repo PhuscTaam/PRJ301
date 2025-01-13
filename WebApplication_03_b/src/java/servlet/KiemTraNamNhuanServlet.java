156bc99745163999b2cb53ce0a1b5956dc1eb62d
PhuscTaam <tam732002@gmail.com>
2025-01-13 13:52:35 +0700

    update
    update

---
+++ WebApplication_03_b/src/java/servlet/KiemTraNamNhuanServlet.java
@@ -0,0 +1,89 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.time.Year;
+import java.util.Date;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author tam73
+ */
+@WebServlet(name = "KiemTraNamNhuanServlet", urlPatterns = {"/NamNhuan"})
+public class KiemTraNamNhuanServlet extends HttpServlet {
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet KiemTraNamNhuanServlet</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            // Date date = new Date();
+            //out.println("Nam hien tai la: "+date.getYear()+1900);           
+            int year = Year.now().getValue();
+            out.println("Nam hien tai la: "+(year));
+            //in ra nam nhuan hay nam khong nhuan
+            if (year %4 ==0) {
+                out.println("<p>Kết quả: Đây là năm nhuận.</p>");
+            } else {
+                out.println("<p>Kết quả: Đây không phải là năm nhuận.</p>");
+            }
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
